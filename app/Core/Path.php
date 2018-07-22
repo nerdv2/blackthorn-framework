9106252d6aabbd1fad1a61476e4788993ffaacfc
nerdv2 <gema_wardian@hotmail.com>
2018-07-22 22:38:03 +0700

    Added isUserLocalhost function.

--- app/Core/Path.php
+++ app/Core/Path.php
@@ -67,6 +67,17 @@ class Path
     }
 
     /**
+     * isUserLocalhost function.
+     *
+     * @access public
+     * @return bool
+     */
+    public function isUserLocalhost()
+    {
+        return in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', "::1"));
+    }
+
+    /**
      * getSmartyCachePath function.
      *
      * @access public
